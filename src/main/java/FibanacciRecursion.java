80161ba3bdbf4f90f9590eeae5b80c8f438e2f29
phantom-mayavi <phantom.mayavi@gmail.com>
2024-09-07 19:44:53 -0400

    fibonacci recursion

--- src/main/java/FibanacciRecursion.java
+++ src/main/java/FibanacciRecursion.java
@@ -1,8 +1,10 @@
 public class FibanacciRecursion {
     public static void main(String[] args) {
         FibanacciRecursion myObj = new FibanacciRecursion();
-//        myObj.fibinacciAdd(0, 1, 10);
         int count = 10;
+        System.out.println("Fibanacci Logic 1....");
+        myObj.fibinacciAdd(0, 1, count);
+        System.out.println("Fibanacci Logic 1....");
         for (int i = 0; i < count; i++) {
             System.out.println(myObj.fibinacciAdd2(i));;
         }
